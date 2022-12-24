d5363a3d373405c2eb659e688a27758a5ef87b71
joe <>
2022-12-24 22:55:14 +0800

    綁定登入頁面事件

--- src/login/Login.tsx
+++ src/login/Login.tsx
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Login() {
+  
+  const [username, setUsername] = useState("joe");
+  const [password, setPassword] = useState("test");
+
+  function handleUsernameChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setUsername(event.target.value);
+  }
+
+  function handlePasswordChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setPassword(event.target.value);
+  }
+
+  function login(e: React.SyntheticEvent) {
+    e.preventDefault();
+    console.log(username);
+    console.log(password);
+  }
+
+  function createAccount() {
+    console.log('createAccount');
+  }
+
   return (
     <div className="relative bg-cover bg-center bg-no-repeat bg-gradient-to-b from-blue-500 to-blue-400 opacity-75">
       <div className="z-50 mx-0 min-h-screen justify-center sm:flex sm:flex-row">
         <div className="z-10 flex flex-col self-center p-10 sm:max-w-5xl xl:max-w-2xl">
           <div className="hidden flex-col self-start text-white lg:flex">
-            <h1 className="mb-3 mt-3 text-5xl font-bold">Hi ? Welcome Back !</h1>
+            <h1 className="mb-3 mt-3 text-5xl font-bold">
+              Hi ? Welcome Back !
+            </h1>
             <p className="pr-3">
               Lorem ipsum is placeholder text commonly used in the graphic,
               print, and publishing industries for previewing layouts and visual
@@ -20,16 +44,19 @@ function Login() {
               <h3 className="text-2xl font-semibold text-gray-800">Sign In</h3>
               <p className="text-gray-500">Please sign in to your account.</p>
             </div>
-            <form>
+            <form onSubmit={login}>
               <div className="space-y-5">
                 <div className="space-y-2">
                   <label className="text-sm font-medium tracking-wide text-gray-700">
                     UserName
                   </label>
                   <input
+                    id="username"
                     className="w-full rounded-lg border border-gray-300 px-4 py-2 text-base focus:border-blue-400 focus:outline-none"
                     type="text"
                     placeholder="Enter your username"
+                    value={username}
+                    onChange={handleUsernameChange}
                   />
                 </div>
                 <div className="space-y-2">
@@ -37,9 +64,12 @@ function Login() {
                     Password
                   </label>
                   <input
+                    id="password"
                     className="w-full content-center rounded-lg border border-gray-300 px-4 py-2 text-base focus:border-blue-400 focus:outline-none"
                     type="password"
                     placeholder="Enter your password"
+                    value={password}
+                    onChange={handlePasswordChange}
                   />
                 </div>
                 <div>
@@ -53,6 +83,7 @@ function Login() {
                 <hr />
                 <div>
                   <button
+                    onClick={createAccount}
                     type="button"
                     className="flex w-full cursor-pointer justify-center rounded-full bg-green-400 p-3 font-semibold tracking-wide text-gray-100 shadow-lg transition duration-500 ease-in hover:bg-green-500"
                   >
